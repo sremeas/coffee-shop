d19af0e3e7581ced7ab9084950f98011a5a1112f
Kakratana <39192742+Kakratana@users.noreply.github.com>
2025-08-11 17:06:39 +0700

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,15 @@ import ProductProvider from './hooks/provider/ProductProvider';
 import ShoppingCartProvider from './hooks/provider/ShoppingCartProvider';
 import ModalProvider from './hooks/provider/ModalProvider/index.tsx';
 import UserAddressProvider from './hooks/provider/UserAddressProvider.tsx';
-import { GOOGLE_OAUTH_CLIENT_ID } from './constants/constants.ts';
+//import { GOOGLE_OAUTH_CLIENT_ID } from './constants/constants.ts';
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 import ScrollToTop from './components/shared/ScrollToTop.tsx';
 import Router from './Router.tsx';
 
 export default function App() {
   return (
-    <GoogleOAuthProvider clientId={GOOGLE_OAUTH_CLIENT_ID}>
+  /*  <GoogleOAuthProvider clientId={GOOGLE_OAUTH_CLIENT_ID}>*/
+    <GoogleOAuthProvider clientId={googleClientId}>
       <BrowserRouter>
         <AuthProvider>
           <UserAddressProvider>
